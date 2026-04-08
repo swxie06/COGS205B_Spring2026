d0d4f04df64050ff74d729ae17ab79aa7552402a
swxie06 <x438814165@gmail.com>
2026-04-08 22:47:06 +0000

    Add csv data

---
+++ scripts/fetch-csvs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+wget -O data.zip 'https://github.com/joachimvandekerckhove/cogs205b-s26/raw/refs/heads/main/modules/02-version-control/files/data.zip' 
+mkdir -p /tmp/extracted_data
+unzip -q data.zip -d /tmp/extracted_data
+DATE=$(date +%F)
+mkdir -p ./data/$DATE
+mv /tmp/extracted_data/*.csv /data/$DATE/
+git add data/ 
+git commit -m "Add csv data"
+git add scripts/fetch-csvs.sh
+git commit -m "Add fetch-csvs script"
+git push
